e7afa1008146942036cb4dbec6a72c3db22c130f
Matteo Tullo <matteotullo@microsoft.com>
2024-11-18 11:35:15 -0800

    added example test logic

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,18 @@
 
 use core::arch::asm;
 
+pub use cortex_m_rt::interrupt;
 use defmt::*;
 use embassy_executor::Spawner;
-pub use cortex_m_rt::interrupt;
-use cortex_m_rt::entry;
 use mimxrt685s_pac as pac;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    defmt::assert!(true);
     let p = pac::Peripherals::take().unwrap();
 
+    info!("Hello world");
     loop {
         unsafe { asm!("nop") };
-        
     }
 }
--- tests/example_test.rs
+++ tests/example_test.rs
@@ -3,9 +3,28 @@
 
 #[cfg(test)]
 #[embedded_test::tests]
+
 mod tests {
+
+    use defmt::assert;
+    use defmt::info;
+    use defmt_rtt as _;
+    use mimxrt685s_pac as pac;
+
     #[test]
     async fn test_example() {
-        
+        let p = pac::Peripherals::take().unwrap();
+
+        info!("Example test");
+        assert!(true);
+    }
+
+    #[test]
+    #[should_panic]
+    async fn test_example_panic() {
+        let p = pac::Peripherals::take().unwrap();
+
+        info!("Should panic");
+        assert!(false);
     }
 }
